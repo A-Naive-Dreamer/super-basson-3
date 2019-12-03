57325c8587e68a53f392809afb501d3685cdb69f
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-12-03 15:00:24 +0700

    Fix broken host.

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     PORT: process.env.PORT,
+    DATABASE_HOST: process.env.DATABASE_HOST,
     DATABASE_NAME: process.env.DATABASE_NAME,
     DATABASE_USER: process.env.DATABASE_USER,
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD
